4ca67034ef8b1a4eca95fbfa0ae26f2b87bf41ce
Virginie PEREIRA <pereira.virgini@gmail.com>
2019-05-24 13:09:25 +0200

    Disconnected Homepage structure creation

---
+++ controller/Controller.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Controller;
+require(__DIR__.'/../config.php');
+
+class Controller {
+    protected $twig;
+
+    public function __construct() {
+        $loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/../view');
+        $twig = new \Twig\Environment($loader);
+
+        $twig->addGlobal('BASE_URL', BASE_URL.'/');
+
+        $this->twig = $twig;
+    }
+
+    public function createImageInFolder($image_input_name, $width, $height, $folder) {
+        $image = new Image($image_input_name);
+        $errors = [];
+
+        if ($image->isExtAllowed()) {
+            $upload_errors = 'invalid_extension';
+        }
+        else {
+            $upload_errors = null;
+        }
+
+        if (is_null($upload_errors)) {
+            $image->resizeAndCompress($image_input_name, $width, $height);
+            $upload_result = $image->upload($folder);
+        } else {
+            $upload_result = null;
+        }
+
+        if (is_null($upload_errors) && $upload_results['upload_status'] == true) {
+            $image_name = $upload_results['image_name'];
+            
+            return $upload_result = [
+                "status" => "success",
+                "image_name" => $image_name,
+            ];
+        } 
+        else {
+
+            $errors[] = $upload_data['upload_errors'];
+
+            if (!empty($upload_results) && !$upload_results['upload_status']) {
+                $errors[] = 'file_not_moved';
+            }
+
+            return $upload_result = [
+                "status" => "errors",
+                "errors" => $errors,
+            ];
+        }
+
+        
+    }
+}
--- index.php
+++ index.php
@@ -2,9 +2,15 @@
 
 require 'composer/vendor/autoload.php';
 use App\Router\Router;
+use App\Controller\HomeController;
 
 $router = new Router($_GET['url']);
-$router->addRoute('GET', '/', function(){ echo 'Homepage';});
+
+$router->addRoute('GET', '/', function(){ 
+    $home_controller = new HomeController();
+    $home_controller->displayDisconnectedHome();
+});
+
 $router->addRoute('GET', '/test', function(){echo 'Page de Test';});
 
 $router->run();
